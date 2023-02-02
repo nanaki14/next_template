534e9ce585581180fc6aa334e46c56cf92fd0bb4
nanaki14 <masatochiba0122@gmail.com>
2023-02-02 22:01:23 +0900

    fix: libs version up

--- src/utils/$path.ts
+++ src/utils/$path.ts
@@ -1,16 +1,11 @@
-/* eslint-disable */
-// prettier-ignore
 export const pagesPath = {
   $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
 }
 
-// prettier-ignore
 export type PagesPath = typeof pagesPath
 
-// prettier-ignore
 export const staticPath = {
   favicon_ico: '/favicon.ico'
 } as const
 
-// prettier-ignore
 export type StaticPath = typeof staticPath
